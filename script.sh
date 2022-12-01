7979bf38b5d93e3d65df14845a9d22f08824d88f
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-12-01 20:01:47 +0000

    feat: working, but creates too many child processes, limit is 2666. Need to turn it into a function that is recalled, rather than a script calling itself

--- script.sh
+++ script.sh
@@ -4,32 +4,18 @@ BLOCK=$1
 
 NONCE=$2
 
-# hash=$(sha512sum $BLOCK)
-
 cat $BLOCK > temp_block.txt
 echo \ >> temp_block.txt
 echo $NONCE >> temp_block.txt
 
 hash_value=$(sha512sum temp_block.txt)
 
-START=${hash_value:0:1}
+START=${hash_value:0:2}
 
-if [[ "$START" == "0" ]]; then
-  echo fine
+if [[ "$START" == "00" ]]; then
+  echo $NONCE loops
   echo $hash_value
 else 
   NONCE=$((NONCE + 1))
-  echo $NONCE
-  echo Not fine
-  echo Calling script again
   ./script.sh $1 $NONCE
 fi
-
-# sha512sum $BLOCK
-
-# nonce=0
-# Create function
-# Add nonce to BLOCK (how?)
-# Do sha51sum on nonce and BLOCK
-# If hash value has 2 zeroes at the start, then stop, and print
-# Else increment nonce by 1
