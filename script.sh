fece3173a5dd4d5163f67ce65b10b009939d1382
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-12-03 18:43:59 +0000

    feat: script working, port to ruby and test drive next

--- script.sh
+++ script.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
 BLOCK=$1
-
 NONCE=$2
 
-cat $BLOCK > temp_block.txt
-echo \ >> temp_block.txt
-echo $NONCE >> temp_block.txt
+compute () {
 
-hash_value=$(sha512sum temp_block.txt)
+  cat $BLOCK > temp_block.txt
+  echo \ >> temp_block.txt
+  echo $NONCE >> temp_block.txt
 
-START=${hash_value:0:2}
+  hash_value=$(sha512sum temp_block.txt)
 
-if [[ "$START" == "00" ]]; then
-  echo $NONCE loops
-  echo $hash_value
-else 
-  NONCE=$((NONCE + 1))
-  ./script.sh $1 $NONCE
-fi
+  START=${hash_value:0:3}
+
+  if [[ "$START" == "000" ]]; then
+    echo $NONCE loops
+    echo $hash_value
+  else 
+    NONCE=$((NONCE + 1))
+    compute $1 $NONCE
+  fi
+
+}
+
+compute $BLOCK $NONCE
+
+
