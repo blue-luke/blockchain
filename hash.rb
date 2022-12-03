88f3cc9c673efb4d68979fe74c82aa73bb895582
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-12-03 19:04:17 +0000

    green: returning a string

---
+++ hash.rb
@@ -0,0 +1,3 @@
+def calculate(string)
+  return string
+end
--- spec/hash_spec.rb
+++ spec/hash_spec.rb
@@ -1,5 +1,7 @@
+require './hash.rb'
+
 describe "hash function" do 
   it "returns the string passed to it" do
-    expect(calculate("Jane pays John 10BTC")).be_kind_of(string)
+    expect(calculate("Jane pays John 10BTC")).to eq ("Jane pays John 10BTC")
   end
 end
