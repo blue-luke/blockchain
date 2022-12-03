df061ed9fe19d24b537e4bef931aad2439163132
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-12-03 19:06:03 +0000

    refactor: returning a string

--- spec/hash_spec.rb
+++ spec/hash_spec.rb
@@ -1,6 +1,6 @@
 require './hash.rb'
 
-describe "hash function" do 
+describe "calculate" do 
   it "returns the string passed to it" do
     expect(calculate("Jane pays John 10BTC")).to eq ("Jane pays John 10BTC")
   end
