bed84ef2b0558e946ef03709d972a2cbdffa56ae
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-12-03 19:20:26 +0000

    refactor: output is 64 characters

--- spec/hash_spec.rb
+++ spec/hash_spec.rb
@@ -1,7 +1,7 @@
 require './hash.rb'
 
 describe "calculate" do 
-  it "returns a 64 character output - a hash value" do
+  it "returns a 64 character output" do
     expect(calculate("Jane pays John 10BTC").length).to eq (64)
   end
 end
