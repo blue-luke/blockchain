779fd9bff06b28454e8c84d7826bd1cee9125ef8
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-12-04 15:09:06 +0000

    refactor: iterating through nonces

--- hash.rb
+++ hash.rb
@@ -1,8 +1,18 @@
 require 'digest'
 
-def calculate(string, start_condition)
-  nonce = 1
+nonce=1
+
+def calculate(string, start_condition, nonce=1)
   block = string + nonce.to_s
-  intermediate_hash = Digest::SHA256.hexdigest block
-  final_hash = (start_condition + intermediate_hash)[0,64]
+
+  hash_value = Digest::SHA256.hexdigest block
+
+  if hash_value[0,1] == start_condition
+    return hash_value
+  else
+    nonce+=1
+    calculate(string, start_condition, nonce)
+  end
+  # intermediate_hash = Digest::SHA256.hexdigest block
+  # final_hash = (start_condition + intermediate_hash)[0,64]
 end
--- spec/hash_spec.rb
+++ spec/hash_spec.rb
@@ -8,7 +8,7 @@ describe "calculate" do
     expect(calculate("Jane pays John 10BTC", "0")).not_to eq(calculate("Jane pays Alan 10BTC", "0"))
   end
   it "finds a hash value that has one 0 at the beginning" do
-    # Where second argument is start of has value condition
+    # Where second argument is value of the start condition
     hash = calculate("Jane pays John 10BTC", "0")
     expect(hash[0,1]).to eq("0")
   end
