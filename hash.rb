bbd1e12699b88a864b7b7eb2b56e1c752d9eb52a
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-12-04 14:38:41 +0000

    refactor: different nonce, different output. Using sha256 hexdigest

--- hash.rb
+++ hash.rb
@@ -1,4 +1,8 @@
+require 'digest'
+
 def calculate(string, nonce)
-  output= nonce.to_s + string + string + string + "abcd"
-  return output[0, 64]
+  block = string + nonce.to_s
+  return Digest::SHA256.hexdigest block
+  # output= nonce.to_s + string + string + string + "abcd"
+  # return output[0, 64]
 end
