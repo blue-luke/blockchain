998a7241b5d5cfed67f032ca327837847c4c0746
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-12-04 14:51:25 +0000

    red: hash value that has one 0 at beginning

--- spec/hash_spec.rb
+++ spec/hash_spec.rb
@@ -7,4 +7,9 @@ describe "calculate" do
   it "returns a different output for a different input" do
     expect(calculate("Jane pays John 10BTC")).not_to eq(calculate("Jane pays Alan 10BTC"))
   end
+  it "finds a hash value that has one 0 at the beginning" do
+    # Where second argument is number of zeroes at beginning
+    hash = calculate("Jane pays John 10BTC", 1)
+    expect(hash[0,1]).to eq("0")
+  end
 end
