563f3db3ceafdfb832b5d14c42eaf1c86b4b6575
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-12-04 17:36:28 +0000

    refactor: iterating rather than recursing, to avoid stack and process limits

--- hash.rb
+++ hash.rb
@@ -2,15 +2,23 @@ require 'digest'
 
 def calculate(string, start_condition, nonce=1)
 
-  block = string + nonce.to_s
-
-  hash_value = Digest::SHA256.hexdigest block
-
-  if hash_value[0,start_condition.length] == start_condition
-    print 'Nonce was' + ' ' + nonce.to_s 
-    return hash_value
-  else
+  nonce=1
+  loop do
+    block = string + nonce.to_s
+    hash_value = Digest::SHA256.hexdigest block
+    if hash_value[0,start_condition.length] == start_condition
+      print 'Nonce is ' + nonce.to_s
+      return hash_value
+      break
+    end
     nonce+=1
-    calculate(string, start_condition, nonce)
   end
+
+  # if hash_value[0,start_condition.length] == start_condition
+  #   print 'Nonce was' + ' ' +  nonce.to_s 
+  #   return hash_value
+  # else
+  #   nonce+=1
+  #   calculate(string, start_condition, nonce)
+  # end
 end
